8808ad1aeeb5dcbcde8c58af4bf85c134659c831
huangwenxia <huangwenxia@prophetech.cn>
2019-11-16 20:23:53 +0800

    yh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import Sidebar from './pages/Layout/sidebar/Sidebar';
+// import Sidebar from './pages/Layout/sidebar/Sidebar';
 import {BrowserRouter, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter basename='/'>
-            <Sidebar/>
-       </BrowserRouter>
+
     </div>
   );
 }
